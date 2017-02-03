a5d14fd75205a77f5500ee9618f99f4e04396171
Jun Ito <jun_ito@voyagegroup.com>
2017-02-03 10:55:06 +0900

    Update: 一通り動くようになった

--- src/component.js
+++ src/component.js
@@ -8,29 +8,11 @@ AFRAME.registerComponent('my-component', {
     schema: {},
     multiple: false,
     init: function() {
-        ["hello", "component"].map(v => console.log(v));
+        ["hello", "component", "!!!"].map(v => console.log(v));
     },
     update: function(oldData) {},
     remove: function() {},
     // tick: function (t) { },
     pause: function() {},
     play: function() {}
-});
-
-document.addEventListener("loaded", function() {
-	console.log('loaded');
-
-    var main = function() {
-        // ES6 test
-        ["hello", "world", "!"].map(n => console.log(n));
-    }
-
-
-    var scene = document.querySelector("a-scene");
-
-    if (scene.hasLoaded) {
-        main();
-    } else {
-        scene.addEventListener('loaded', main);
-    }
 });
