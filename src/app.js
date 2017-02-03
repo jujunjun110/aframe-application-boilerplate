39bba639c0c44a8301a7daf00150c3b4472d120a
Jun Ito <jun_ito@voyagegroup.com>
2017-02-03 10:28:38 +0900

    Update: 1jsファイルでやる方法を発見

--- src/app.js
+++ src/app.js
@@ -1,12 +1,36 @@
-var main = function () {
-	// ES6 test
-	["hello", "world", "!"].map(n => console.log(n));
+/* here you write your components. */
+
+if (typeof AFRAME === 'undefined') {
+    throw new Error('Component attempted to register before AFRAME was available.');
 }
 
+AFRAME.registerComponent('my-component', {
+    schema: {},
+    multiple: false,
+    init: function() {
+        ["hello", "component"].map(v => console.log(v));
+    },
+    update: function(oldData) {},
+    remove: function() {},
+    // tick: function (t) { },
+    pause: function() {},
+    play: function() {}
+});
 
-var scene = document.querySelector("a-scene");
-if (scene.hasLoaded) {
-    main();
-} else {
-    scene.addEventListener('loaded', main);
-}
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('loaded');
+
+    var main = function() {
+        // ES6 test
+        ["hello", "world", "!"].map(n => console.log(n));
+    }
+
+
+    var scene = document.querySelector("a-scene");
+
+    if (scene.hasLoaded) {
+        main();
+    } else {
+        scene.addEventListener('loaded', main);
+    }
+});
