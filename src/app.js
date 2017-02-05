8f122e20b33dc9c1df6727a94c6fa14df1a6ef81
Jun Ito <jun_ito@voyagegroup.com>
2017-02-05 18:35:41 +0900

    Delete: needless error

--- src/app.js
+++ src/app.js
@@ -2,12 +2,8 @@
 require('./component.js')
 
 document.addEventListener('DOMContentLoaded', function () {
-    if (typeof AFRAME === 'undefined') {
-        throw new Error('Component attempted to register before AFRAME was available.')
-    }
-
     /* here you write main logic. */
-
     // ES6 notation
     ['hello', 'world', '!'].map((v) => console.log(v))
 })
+
