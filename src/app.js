8799134e8e1698a214991ff919114e1d1bfffd35
Jun Ito <jun_ito@voyagegroup.com>
2017-02-12 21:20:58 +0900

    Update: refactering

--- src/app.js
+++ src/app.js
@@ -1,13 +1,12 @@
-/* here you import original components. */
+/* here you require original components. */
 require('./component.js')
 
 document.addEventListener('DOMContentLoaded', function () {
     if (typeof AFRAME === 'undefined') {
-        throw new Error('Component attempted to register before AFRAME was available.')
+        throw new Error('Application started before AFRAME was available.')
     }
 
     /* here you write main logic. */
-
     // ES6 notation
     ['hello', 'world', '!'].map((v) => console.log(v))
 })
