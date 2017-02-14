a13473ead8c2b7c63f0aea8100397b236026e126
Jun Ito <jun_ito@voyagegroup.com>
2017-02-15 00:29:55 +0900

    Add: shader

--- src/app.js
+++ src/app.js
@@ -1,12 +1,13 @@
 /* here you require original components. */
 require('./component.js')
+require('./shader.js')
 
 document.addEventListener('DOMContentLoaded', function () {
     if (typeof AFRAME === 'undefined') {
         throw new Error('Application started before AFRAME was available.')
     }
-
     /* here you write main logic. */
     // ES6 notation
     ['hello', 'world', '!'].map((v) => console.log(v))
 })
+
