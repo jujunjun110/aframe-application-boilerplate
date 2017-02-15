0744c5e5ce4d33135d5daf6697060a0d57e255c9
Jun Ito <jun_ito@voyagegroup.com>
2017-02-15 18:55:26 +0900

    Update: comment

--- src/shader.js
+++ src/shader.js
@@ -2,7 +2,7 @@ if (typeof AFRAME === 'undefined') {
     throw new Error('Component attempted to register before AFRAME was available.')
 }
 
-/* here you write your components. */
+/* here you write your shader. */
 AFRAME.registerShader('my-shader', {
     schema: {},
     vertexShader: require('./shader/default.vert')(),
